73ac588cad0cf2013153a5f17222d360c3cb00cb
PeterRosner <prosner97@gmail.com>
2021-10-15 07:02:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-x7d7da
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-x7d7da)
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.css";
+
+function Product(props) {
+  return(<div>
+    {props.Name} {props.Id} {props.datePurchased} £{props.price}
+  </div>)
+
+}
+
+export default function App() {
+  return (
+    
+    /*<div style={{color:"red", fontFamily:"Arial"}}>
+    css now in style.css*/
+    <div>
+        <Product Name="Toaster"  Id="TO1234" datePurchased="5/4/21" price="4.00"/>
+
+    </div>
+  );
+}
